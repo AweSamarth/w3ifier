a62d4d2952343bc346c3f64a5f3f543f13d821cb
Samarth Saxena <samarthsaxena1672003@gmail.com>
2022-12-31 23:14:46 +0530

    on track

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,46 +1,45 @@
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
 import type { AppProps } from 'next/app';
-import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit';
+import {
+  darkTheme,
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
 import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { mainnet, polygon, optimism, arbitrum, goerli } from 'wagmi/chains';
+import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
-
-const { chains, provider, webSocketProvider } = configureChains(
+import {useAccount} from "wagmi"
+const { chains, provider } = configureChains(
+  [mainnet, polygon, optimism, arbitrum],
   [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
-  ],
-  [
-    alchemyProvider({
-      // This is Alchemy's default API key.
-      // You can get your own at https://dashboard.alchemyapi.io
-      apiKey: '_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC',
-    }),
-    publicProvider(),
+    alchemyProvider({ apiKey: "gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC" }),
+    publicProvider()
   ]
-);
-
+); 
 const { connectors } = getDefaultWallets({
-  appName: 'RainbowKit App',
-  chains,
+  appName: 'My RainbowKit App',
+  chains
 });
-
 const wagmiClient = createClient({
   autoConnect: true,
   connectors,
-  provider,
-  webSocketProvider,
-});
+  provider
+})
+
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
+      <RainbowKitProvider modalSize='compact' chains={chains} theme={darkTheme({
+      accentColor: '#a100fe',
+      accentColorForeground: 'white',
+      borderRadius: 'medium',
+      fontStack: 'system',
+      overlayBlur: 'small',
+    })}>
         <Component {...pageProps} />
       </RainbowKitProvider>
     </WagmiConfig>
---
+++ pages/checking.tsx
@@ -0,0 +1,112 @@
+import { useEffect, useState } from "react";
+import styles from "../styles/another.module.css";
+
+function MyComponent() {
+  const [text, setText] = useState("Initial text");
+
+  useEffect(() => {
+    
+    const handleScroll = () => {
+      if (window.scrollY > window.innerHeight) {
+        console.log("scrolled");
+        setText("New text");
+      } else {
+        setText("Initial text");
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div>
+      <div className={styles.page}>
+        <div style={{ position: "fixed", top: 0 }}>{text}</div>
+      </div>
+
+      <div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </div>
+    </div>
+  );
+}
+
+export default MyComponent;
